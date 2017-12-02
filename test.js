72d188027bfa393f974fb407d52e5c3fd4539c6c
Mr Speaker <mrspeaker@gmail.com>
2017-12-02 13:32:57 -0500

    use mth

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 import pop from "./src";
-const { Game, KeyControls } = pop;
+const { Game, KeyControls, math } = pop;
 
 const game = new Game(800, 600, true);
 const { scene, w, h } = game;
@@ -14,7 +14,7 @@ pos.y = h / 2 - 100;
 for (let i = 0; i < 20; i++) {
   scene.make
     .sprite("res/images/greona.png")
-    .pos.set((Math.random() * w) | 0, (Math.random() * h) | 0);
+    .pos.set(math.rand(0, w), math.rand(0, h));
 }
 
 game.run((dt, t) => {
