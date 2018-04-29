54de836275573949735fdf727f011853d01a6f9b
Mr Speaker <mrspeaker@gmail.com>
2018-04-29 12:26:24 -0400

    testing notes

--- ex/test.js
+++ ex/test.js
@@ -1,5 +1,4 @@
 import pop from "../src/index.js";
-console.log(pop);
 const { Game, KeyControls, math, Sprite, TileSprite, Texture } = pop;
 
 const useWebGl2 = true;
---
+++ tests/rendererChildren.js
@@ -0,0 +1,4 @@
+// TODO: test that both Containers and Children will exit early from the renderer
+// if either `visible == false` OR `alpha == 0`.
+// Also test the `==` vs `===`. I think on of the tests is intentionally testing a
+// falsy value with `==`. If so, document it and test it.
